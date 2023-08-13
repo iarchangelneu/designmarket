3ded8fce56e7c838fd8b50d6f0797b98b7289955
Valera <iarchangel@bk.ru>
2023-08-13 20:21:04 +0300

    321312

--- pages/success.vue
+++ pages/success.vue
@@ -7,6 +7,45 @@
         </div>
     </div>
 </template>
+<script>
+import global from '~/mixins/global';
+import axios from 'axios';
+export default {
+    mixins: [global],
+    data() {
+        return {
+            pathUrl: 'https://d-market.kz',
+        }
+    },
+    methods: {
+        sendRequest(reference) {
+            const token = this.getAuthorizationCookie();
+            const path = `${this.pathUrl}/api/money/success/${reference}`;
+            axios.defaults.headers.common['Authorization'] = `Token ${token}`;
+            axios
+                .get(path)
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+    },
+    mounted() {
+        const url = window.location.href;
+        const match = url.match(/order_pay_(\d+)/);
+
+        if (match) {
+            this.extractedValue = match[0];
+            console.log(this.extractedValue);
+
+            this.sendRequest(match[0])
+        }
+    },
+
+}
+</script >
 <script setup>
 
 useSeoMeta({
