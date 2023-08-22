54aa6e40dcb82144fb46dc5a603b74b99360c347
Valera <iarchangel@bk.ru>
2023-08-22 10:40:54 +0300

    fff

--- pages/failure.vue
+++ pages/failure.vue
@@ -27,7 +27,7 @@ export default {
                 .get(path)
                 .then(response => {
                     console.log(response)
-                    if (response.status == 201) {
+                    if (response.status == 200) {
                         window.location.href = '/'
                     }
                 })
